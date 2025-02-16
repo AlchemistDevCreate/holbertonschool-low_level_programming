b15426d84c476eae58760bfff08653dac72c04f6
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-02-16 15:58:52 -0500

    Function that encodes a string using rot13.

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: String to encode.
+ * Return: Pointer to the encoded string.
+ */
+
+char *rot13(char *str)
+{
+	char *ptr = str;
+	char original[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char replacement[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+
+	while (*ptr != '\0')
+	{
+		for (i = 0; original[i] != '\0'; i++)
+		{
+			if (*ptr == original[i])
+			{
+				*ptr = replacement[i];
+				break;
+			}
+		}
+		ptr++;
+	}
+
+	return (str);
+}
