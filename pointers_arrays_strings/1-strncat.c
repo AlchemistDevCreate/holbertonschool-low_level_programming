6802cd9972852acf38507dd56cd75f4ac2ed025b
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-02-16 15:26:07 -0500

    Function that copies a string.

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes from src.
+ * @dest: the destination string to which src will be appended.
+ * @src: the source string to be appended to dest.
+ * @n: the maximum number of bytes to be used from src.
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
