858e57f2b538b3daf9977cfd4de020e0213848fc
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-04-27 22:18:04 -0500

    Create a program that generates random valid passwords for the program 101-crackme.

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,24 +2,64 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - Entry point
- * Return: Always 0
- */
-int main(void)
-{
-	int sum = 0;
-	char c;
+#define PASSWORD_LEN 8
 
+/**
+ * Genera un caracter aleatorio válido para la contraseña.
+
+ * Retorno: El caracter aleatorio generado.
+*/
+
+char generate_random_char()
+{
+	int random_number = rand() % 94 + 33;
+	return ((char) random_number);
+}
+
+/**
+ * Genera una contraseña completa utilizando caracteres aleatorios válidos.
+ * Retorno: Puntero a la contraseña generada. La memoria debe liberarse con free().
+*/
+
+char *generate_password()
+{
+	int i;
+	char *password = malloc(PASSWORD_LEN + 1);
+	if (!password)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < PASSWORD_LEN; i++)
+	{
+		password[i] = generate_random_char();
+	}
+
+	password[PASSWORD_LEN] = '\0';
+
+	return (password);
+}
+
+/**
+ *  Función principal del programa.
+*/
+
+int main()
+{
+	int i;
 	srand(time(NULL));
 
-	while (sum < 2772)
+	for (i = 0; i < 10; i++)
 	{
-		c = rand() % 94 + 33;
-		if (sum + c <= 2772)
+		char *password = generate_password();
+		if (password)
 		{
-			printf("%c", c);
-			sum += c;
+			printf("%s\n", password);
+			free(password);
+		}
+		else
+		{
+			printf("Error al generar contraseña.\n");
 		}
 	}
 
