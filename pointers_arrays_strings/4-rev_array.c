379c0e508887329766735541c5cf2529a779f9e2
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-05-06 20:52:22 -0500

    Betty Style

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,17 +9,16 @@
 
 void reverse_array(int *a, int n)
 {
-		int *start = a;
-		int *end = a + n - 1;
-		int temp;
+	int *start = a;
+	int *end = a + n - 1;
+	int temp;
 
-		while (start < end)
-		{
-			temp = *start;
-			*start = *end;
-			*end = temp;
-
-			start++;
-			end--;
+	while (start < end)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
 		}
 }
