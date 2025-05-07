4d20e778c0ce6aa2545aa45969f455ca0327b154
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-05-06 20:54:04 -0500

    Betty Style

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,16 @@
 
 char *string_toupper(char *str)
 {
-		char *ptr = str;
+	char *ptr = str;
 
-		while (*ptr != '\0')
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
 		{
-			if (*ptr >= 'a' && *ptr <= 'z')
-			{
-				*ptr = *ptr - 'a' + 'A';
-			}
-			ptr++;
+			*ptr = *ptr - 'a' + 'A';
 		}
+		ptr++;
+	}
 
-		return (str);
+	return (str);
 }
