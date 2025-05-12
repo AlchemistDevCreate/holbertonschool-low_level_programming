7a4efc5d4bc1e8b9680b889e70898d505e01747c
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-05-11 21:14:58 -0500

    Betty Style

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,17 +11,17 @@
 
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return (s);
-        }
-        s++;
-    }
-    if (*s == c)
-    {
-        return (s);
-    }
-    return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
 }
