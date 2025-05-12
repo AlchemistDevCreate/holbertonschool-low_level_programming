eace08a3692bdb4541702af12bc1f6a6ed5d7c06
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-05-11 20:43:22 -0500

    Betty Style

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+    int n;
+    int a[5];
+    int *p;
 
-	a[2] = 1024;
-	p = &n;
+    a[2] = 1024;
+    p = &n;
 
-	*(p + (&a[2] - &n)) = 98;
+    *(p + (&a[2] - &n)) = 98;
 
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
