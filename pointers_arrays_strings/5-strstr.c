d70a7985af093658c1c4487de84e60f72b849fad
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-05-11 21:22:44 -0500

    Betty Style

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,22 +12,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    char *h, *n;
+	char *h, *n;
 
-    while (*haystack)
-    {
-        h = haystack;
-        n = needle;
-        while (*n && *h == *n)
-        {
-            h++;
-            n++;
-        }
-        if (!*n)
-        {
-            return (haystack);
-        }
-        haystack++;
-    }
-    return (NULL);
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+		while (*n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (!*n)
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
 }
