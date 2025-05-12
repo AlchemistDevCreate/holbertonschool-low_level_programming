d395b97e083aaa893e77df6b091ea58f00c83ba9
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-05-11 21:20:14 -0500

    Betty Style

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,26 +10,26 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i, match;
+	unsigned int count = 0;
+	int i, match;
 
-    while (*s)
-    {
-        match = 0;
-        for (i = 0; accept[i]; i++)
-        {
-            if (*s == accept[i])
-            {
-                count++;
-                match = 1;
-                break;
-            }
-        }
-        if (!match)
-        {
-            break;
-        }
-        s++;
-    }
-    return (count);
+	while (*s)
+	{
+		match = 0;
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				count++;
+				match = 1;
+				break;
+			}
+		}
+		if (!match)
+		{
+			break;
+		}
+		s++;
+	}
+	return (count);
 }
