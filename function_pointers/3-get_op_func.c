daa39f4c50632b146285820f8a345d77b614f9ce
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-06-08 19:54:55 -0500

    Some errors

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,25 +8,18 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    int i = 0;
-    
-    if (s == NULL || strlen(s) != 1)
-        return (NULL);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s)
-            return (ops[i].f);
-        i++;
-    }
-    return (ops[i].f);
+	return (ops[i].f);
 }
